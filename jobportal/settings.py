4734528df2ea85ed02c534774f393d2df53aa972
Hitesh Kumar <rbnritik07@gmail.com>
2026-08-14 11:17:06 +0530

    Configure Neon PostgreSQL

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -85,8 +85,8 @@ WSGI_APPLICATION = 'jobportal.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
         conn_max_age=600,
+        conn_health_checks=True,
     )
 }
 
